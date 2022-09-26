0e1a9a7e395e3ed16f4939627259b29c2ab0b983
Antonio Pezzuto <antonio.pezzuto@pagopa.it>
2022-09-26 14:41:33 +0200

    [PN-2099] File spostati

--- src/main/java/it/pagopa/pn/logsaver/services/support/ExportAudit.java
+++
@@ -1,6 +0,0 @@
-package it.pagopa.pn.logsaver.services.support;
-
-@FunctionalInterface
-public interface ExportAudit<F, O, R, D> {
-  public void export(F folder, O file, R retention, D logDate);
-}
