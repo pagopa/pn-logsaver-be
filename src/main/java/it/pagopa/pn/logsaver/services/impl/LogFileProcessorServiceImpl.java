e5a3726df0f382215c7ef1e2adfb756a3ae150a4
paolavitidicaraffa <paola.vitidicaraffa@dgsspa.com>
2026-03-19 17:25:48 +0100

    bug/PN-18226 - eliminati UAT log

--- src/main/java/it/pagopa/pn/logsaver/services/impl/LogFileProcessorServiceImpl.java
+++ src/main/java/it/pagopa/pn/logsaver/services/impl/LogFileProcessorServiceImpl.java
@@ -44,10 +44,8 @@ public class LogFileProcessorServiceImpl implements LogFileProcessorService {
     // notevolmente in tempi di esecuzione.
     List<LogFileReference> fileList = fileStream.collect(Collectors.toList());
     log.info("Total files {}", fileList.size());
-    log.info("UAT - process start date={} filesToProcess={}", dailyCtx.logDate(), fileList.size());
-
     if (fileList.isEmpty()) {
-      log.warn("UAT - process date={} file list is empty, no files will be processed", dailyCtx.logDate());
+      log.warn("process date={} file list is empty, no files will be processed", dailyCtx.logDate());
     }
 
     log.info("Start processing file");
@@ -73,20 +71,18 @@ public class LogFileProcessorServiceImpl implements LogFileProcessorService {
       log.warn("ATTENZIONE: Alcuni file sono andati perduti nel parallelStream! ({} file non pervenuti)",
               fileList.size() - (processedCount.get() + errorCount.get()));
     }
-    log.info("UAT - process parallel processing completed date={}", dailyCtx.logDate());
 
     log.info("Start creating files");
     List<AuditFile> groupedAudit = createAuditFile(dailyCtx);
-    log.info("Files created {}", groupedAudit.size());
-    log.info("UAT - process end date={} auditFilesCreated={}", dailyCtx.logDate(), groupedAudit.size());
+    log.info("Files created {} - process end date={}", groupedAudit.size(), dailyCtx.logDate());
+
     return groupedAudit;
 
   }
 
   private void downloadFilterWrite(LogFileReference itemLog, DailyContextCfg dailyCtx) {
     LogSaverUtils.initMDC(dailyCtx);
-    log.debug("Dowload file {}", itemLog.getS3Key());
-    log.debug("UAT - downloadFilterWrite start s3Key={} type={} date={}", itemLog.getS3Key(), itemLog.getType(), dailyCtx.logDate());
+    log.debug("downloadFilterWrite start - Dowload file s3Key={} type={} date={}", itemLog.getS3Key(), itemLog.getType(), dailyCtx.logDate());
     // Download file dal bucket
     try (InputStream content = s3Service.getContent(itemLog.getS3Key());) {
       itemLog.setContent(content);
@@ -97,7 +93,6 @@ public class LogFileProcessorServiceImpl implements LogFileProcessorService {
 
     } catch (IOException e) {
       log.warn("Unexpected error closing input stream");
-      log.error("UAT - downloadFilterWrite error on s3Key={} date={}", itemLog.getS3Key(), dailyCtx.logDate(), e);
       throw new UncheckedIOException("writeLog IOException", e);
     } finally {
       LogSaverUtils.clearMdcFromForkThread();
